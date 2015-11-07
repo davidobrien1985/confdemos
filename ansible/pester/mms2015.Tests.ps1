a310291055b6a07d56a9f9910d8dcabfc7fa9772
David <obrien.david@outlook.com>
2015-11-07 18:43:23 +1100

    fixed iis.yml, removed unneccessary files

--- ansible/pester/mms2015.Tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿Describe 'Assert-MockCalled Scope behavior' {
-   
-    It 'Calls New-AzureVM at least once in the It block' {
-        Mock New-AzureVM { }
-        { New-AzureVM }            
-        #Assert-MockCalled New-AzureVM 0
-        Assert-VerifiableMocks
-    }
-}
