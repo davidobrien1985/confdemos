6dffc08c85cdfb370d894839f7a579081bf3b0cc
David <obrien.david@outlook.com>
2015-11-08 19:04:17 +1100

    added standalone Pester tests for Mocking and real tests

--- pester/mms2015_standalone.Tests.ps1
+++ pester/mms2015_standalone.Tests.ps1
@@ -1,3 +1,18 @@
+Function Create-File {
+[CmdletBinding(SupportsShouldProcess=$true)]
+[OutputType('System.IO.FileInfo')]
+param (
+    [switch]$Force = $true,
+    [Parameter(mandatory=$true)]
+    [ValidateSet('File','Directory')]
+    [string]$ItemType,
+    [Parameter(mandatory=$true)]
+    [string]$Path
+)
+    $file = New-Item @PSBoundParameters
+    return $file
+}
+
 Describe 'Demo' {
   Context 'File testing' {
   
@@ -14,6 +29,27 @@ Describe 'Demo' {
 
     It "checks the file's content" {
       'TestDrive:\test.txt' | Should Contain 'This'
+    }
+  }
+
+  Context 'Mocking' {
+    Mock -CommandName New-Item -MockWith {} -Verifiable
+
+    $file = Create-File -Path Testdrive:\bla.txt -ItemType File -Force
+
+    It 'verifies that the command was called' {
+        Assert-VerifiableMocks
+    }
+    It 'checks it has been called only once with correct params' {
+        Assert-MockCalled -CommandName New-Item -Times 1 -Scope Context -ParameterFilter {($Path -eq 'Testdrive:\bla.txt') -and ($ItemType -eq 'File')}
+    }
+  }
+  Context 'Functional test' {
+    It 'checks the actual output' {
+        Mock -CommandName New-Item -MockWith {$file = New-Object -TypeName pscustomobject -Property @{FullName = ''}; $file.FullName = 'Testdrive:\bla.txt'; return $file }
+
+        $file = Create-File -Path Testdrive:\bla.txt -ItemType File -Force
+        $file.FullName | Should Be 'Testdrive:\bla.txt'
     }
   }
 }
